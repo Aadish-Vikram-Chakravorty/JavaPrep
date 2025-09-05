d213019f5261640e129c6584d1e645fef55ce16c
Aadish-Vikram-Chakravorty <aadishchakravorty@gmail.com>
2025-09-05 23:53:47 +0530

    Commit

--- RectangleArea.java
+++ RectangleArea.java
@@ -1,3 +1,21 @@
+
+import java.util.Scanner;
+
 public class RectangleArea {
+    static int calculateArea(int length, int breadth){
+        int area = length*breadth;
+        return area;
+    }
     
+    class Main{
+        public static void main(String args[]){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter length of the rectangle");
+            int l = sc.nextInt();
+            System.out.println("Enter breadth of the rectangle");
+            int b = sc.nextInt();
+            int area = calculateArea(l, b);
+            System.out.println("Area of the rectangle is: " + area); 
+        }
+    }
 }
